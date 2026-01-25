0606b9a67c80b1ec082ca8e45649fdc39d6e254c
juatnegreteg-crypto <juat.negreteg@gmail.com>
2026-01-25 00:50:34 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,3 @@ Route::get('/', function () {
     return view('orders');
 });
 
-Route::get('/check-db', function () {
-    $orders = \App\Models\Order::all();
-
-    return view('check-db', [
-        'total' => $orders->count(),
-        'orders' => $orders,
-        'db_path' => database_path('database.sqlite'),
-    ]);
-});
-;
-
