a3afda4afe21d7143ad5229a8b1e31a1c72623ca
juatnegreteg-crypto <juat.negreteg@gmail.com>
2026-01-24 23:32:50 -0500

    Add route to check database and return orders

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ Route::get('/', function () {
     return view('orders');
 });
 
+Route::get('/check-db', function () {
+    // Esto obtiene todas las órdenes y las muestra en formato bonito
+    return response()->json([
+        'total_registros' => \App\Models\Order::count(),
+        'datos' => \App\Models\Order::all(),
+        'ubicacion_db' => database_path('database.sqlite'),
+    ]);
+});
